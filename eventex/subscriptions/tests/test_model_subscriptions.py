acae0e9dd9b248b0d141c9beb2580a713e66ad86
Luis <ltsj13@gmail.com>
2020-04-13 16:04:29 -0300

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscriptions.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from unittest import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Luis tavares',
+            cpf='12345678901',
+            email='luis@tavares.com',
+            phone='9208-8183')
+
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        '''Subscription must have an auto created_at attr'''
+        self.assertIsInstance(self.obj.created_at, datetime)
