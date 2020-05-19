92fd8c53bac8b957d8256fa86492451125ba2586
Luis <ltsj13@gmail.com>
2020-05-19 17:44:01 -0300

    Speaker

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from eventex.core.models import Speaker
 
 def home(request):
-    speakers = [
-        {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
-        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
-    ]
-    template_name = 'index.html'
-    return render(request, template_name, {'speakers': speakers})
+    speakers = Speaker.objects.all()
+    return render(request, 'index.html', {'speakers': speakers})
+
+
+def speaker_detail(request, slug):
+    template_name = 'core/speaker_detail.html'
+    speaker = get_object_or_404(Speaker, slug=slug)
+    return render(request, template_name, {'speaker': speaker})
