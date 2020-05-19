569f59a0e0af88338694088b03be897784739e19
Luis <ltsj13@gmail.com>
2020-05-19 12:06:58 -0300

    Keynote speakers

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def home(request):
+    speakers = [
+        {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
+        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
+    ]
     template_name = 'index.html'
-    return render(request, template_name)
+    return render(request, template_name, {'speakers': speakers})
