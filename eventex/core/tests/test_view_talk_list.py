5d7f59183d01764552b480df4b216c0df5185ccf
Luis <ltsj13@gmail.com>
2020-07-10 19:26:18 -0300

    Course como abstract base class

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
 
-from eventex.core.models import Talk, Speaker
+from eventex.core.models import Talk, Speaker, Course
 
 
 class TalkListGet(TestCase):
@@ -14,11 +14,17 @@ class TalkListGet(TestCase):
              start='13:00',
              description='Descrição da palestra.')
 
+        c1 = Course.objects.create(title='Título do Curso',
+                                   start='09:00',
+                                   description='Descrição do curso.',
+                                   slots=20)
+
         speaker = Speaker.objects.create(name='Luis Tavares',
                                          slug='luis-tavares',
                                          website='http://luistavares.net')
         t1.speakers.add(speaker)
         t2.speakers.add(speaker)
+        c1.speakers.add(speaker)
 
         self.response = self.client.get(r('talk_list'))
 
@@ -33,22 +39,25 @@ class TalkListGet(TestCase):
             (2, 'Titulo da Palestra'),
             (1, '10:00'),
             (1, '13:00'),
-            (2, '/palestrantes/luis-tavares/'),
-            (2, 'Luis Tavares'),
+            (3, '/palestrantes/luis-tavares/'),
+            (3, 'Luis Tavares'),
             (2, 'Descrição da palestra.'),
+            (1, 'Título do Curso'),
+            (1, '09:00'),
+            (1, 'Descrição do curso.')
         ]
 
         for count, expected in contents:
             with self.subTest():
                 self.assertContains(self.response, expected, count)
 
-
     def test_context(self):
-        variables = ['morning_talks', 'afternoon_talks']
+        variables = ['morning_talks', 'afternoon_talks', 'courses']
 
         for key in variables:
             with self.subTest():
                 self.assertIn(key, self.response.context)
+
 
 class TalkListGetEmpty(TestCase):
     def test_get_empty(self):
